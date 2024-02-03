de3f9dbc1121c04554bbba458f782df406d08963
Andrew <yama.nowrouz@gmail.com>
2024-02-02 19:46:39 -0500

    Update colorscheme.lua
    added code for neovim gruvbox colorscheme, 2/2/2024

--- colorscheme.lua
+++ colorscheme.lua
@@ -1,10 +1,20 @@
+--[[
+vim.cmd.colorscheme 'gruvbox'
+local palette = require 'gruvbox'
+
+local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
+if not status then
+  print("Colorscheme not found!")
+  return
+end
+]]
+
 vim.cmd.colorscheme 'nordic'
 local palette = require 'nordic.colors'
 
 local status, _ = pcall(vim.cmd, "colorscheme nordic")
 if not status then
-  print("Colorsheme not found!")
+  print("Colorscheme not found!")
   return
 end
-
 
