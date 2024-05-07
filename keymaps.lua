31024ba06f3824086be7cce33b84127b9a4e9352
Andrew <yama.nowrouz@gmail.com>
2024-05-07 15:21:06 -0400

    Update keymaps.lua
    5/7/24 -- trying new remaps for motion

--- keymaps.lua
+++ keymaps.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " "
+vim.g.mapleader = " " -- leader = <space>
 
 local keymap = vim.keymap -- for conciseness
 
@@ -6,6 +6,27 @@ local keymap = vim.keymap -- for conciseness
 -- general keymaps
 
 -- normal mode remaps
+keymap.set("n", "k", "i") -- enters insert mode from normal mode
+
+-- normal mode motion
+
+keymap.set("n", "n", "h") -- cursor left 
+keymap.set("n", "e", "j") -- cursor down a terminal line
+keymap.set("n", "u", "k") -- cursor up a terminal line
+keymap.set("n", "i", "l") -- cursor right
+
+keymap.set("n", "h", "e") -- moves cursor to end of word
+
+keymap.set("n", "N", "0") -- moves cursor to beginning of line
+keymap.set("n", "E", "10j") -- moves cursor down 10 lines (=10e)
+keymap.set("n", "U", "10k") -- moves cursor down 10 lines (=10u)
+keymap.set("n", "I", "$") -- moves cursor to the end of the line
+
+keymap.set("n", "W", "10w") -- move forward 10 words
+keymap.set("n", "B", "10b") -- move backwards 10 words
+
+-- normal mode convenience tricks
+
 keymap.set("n", "<leader>nh", ":nohl<CR>") -- clears search highlights
 
 keymap.set("n", "x", '"_x') -- deletes character without copying to register
@@ -15,19 +36,6 @@ keymap.set("n", "<leader>-", "<C-x>") -- increments -1
 
 keymap.set("n", "<leader>w", ":w<CR>") -- space + w = save
 keymap.set("n", "<leader>wq", ":wq<CR>") -- space + wq = save and quit
-
---[[
-keymap.set("n", "m", "h")
-keymap.set("n", "n", "j")
-keymap.set("n", "e", "k")
-keymap.set("n", "i", "l")
-
-keymap.set("n", "f", "e")
-keymap.set("n", "l", "i")
---]]
-
-keymap.set("n", "N", "10j")
-keymap.set("n", "E", "10k")
 
 -- normal mode windows
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
@@ -43,7 +51,7 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
 
 -- insert mode remaps
-keymap.set("i", "hh", "<ESC>") -- n and h to exit text
+keymap.set("i", "hh", "<ESC>") -- h and h to exit insert mode
 
 -- insert mode bracket doublers
 keymap.set("i", "(", "()<esc>i")
@@ -53,12 +61,12 @@ keymap.set("i", "[", "[]<esc>i")
 
 -- visual mode remaps
 
---[[
-keymap.set("v", "m", "h")
-keymap.set("v", "n", "j")
-keymap.set("v", "e", "k")
+keymap.set("v", "n", "h")
+keymap.set("v", "e", "j")
+keymap.set("v", "u", "k")
 keymap.set("v", "i", "l")
 
+--[[
 keymap.set("v", "J", "10j")
 keymap.set("v", "K", "10k")
 --]]
@@ -74,3 +82,5 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+
